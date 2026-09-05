229b0f805c46db59fc93097d991b88c3265774d4
Siddhant Patel <2228.stkabirnrnp@gmail.com>
2026-09-05 19:13:21 +0530

    Add PORT env var support and nodemon dev script

--- backend/index.js
+++ backend/index.js
@@ -71,7 +71,7 @@ app.post("/session/:id/respond", limiter , async (req, res) => {
 
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
